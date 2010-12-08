14f444b4e5c3380245361f9cfad8cc58dd6f0f07
James An <james@jamesan.ca>
2010-12-08 08:42:13 -0500

    by jamesan: Extended JS client.

--- modules/citybudget_js/js/citybudget_js.js
+++ modules/citybudget_js/js/citybudget_js.js
@@ -18,83 +18,41 @@
     $.getJSON(this.apiPath, callback);
   };
 
-  // Drupal.note.create({
-  //   subject: 'Note',
-  //   note: 'A test note',
-  // }, function(r) {console.log(r)});
-
   $(document).ready(function () {
     citybudgetapi.index(function (programs) {
+      var treemap = new Object();
+      treemap.id = 'ID';
+      treemap.name = 'Name';
+      treemap.children = new Array();
       for (var i in programs) {
-        citybudget_append(programs[i].name);
+        var child = '{' +
+          '"children": [],' +
+          '"data": {' +
+            '"$area": ' + programs[i]['2009 Budget'] + ',' +
+          '},' +
+          '"id": "' + programs[i].name + '",' +
+          '"name": "' + programs[i].name + '",' + 
+        '}';
+        var childNode = eval('(' + child + ')');
+        treemap.children.push(childNode);
+        citybudget_append(programs[i]);
       }
-    });
-
-    citybudget_append = function (programName) {
-      var programNode;
-
-      programNode = $('<li class="program"><a href="' + this.location.href + '/' + programName + '">' +
-                       programName + '</a></li>');
-      programNode.appendTo('#citybudget-js');
-    };
-
-
-  /*
-    var subject, text, list, note, note_saved, note_append;
-
-    $('<div id="notetaking"><form>' +
-        '<div class="subject-wrapper"><label for="note-subject">Subject</label></div>' +
-        '<div class="note-wrapper"></div>' + 
-        '<input class="cancel" type="submit" value="Cancel" />' +
-        '<input class="save" type="submit" value="Save" />' +
-      '</form></div>').appendTo('#citybudget-js');
-    subject = $('<input class="subject" type="text" />').appendTo('#notetaking .subject-wrapper');
-    text = $('<textarea class="note" cols="20" rows="5" />').appendTo('#notetaking .note-wrapper');
-    list = $('<ul></ul>').appendTo('#notetaking');
-
-    // Stop the form from submitting
-    $('#notetaking form').submit(function () {
-      return false;
-    });
-
-    $('#notetaking input.cancel').hide().click(function () {
-      note = null;
-      $(this).hide();
-      $(subject).val('');
-      $(text).val('');
-    });
-
-    $('#notetaking input.save').click(function () {
-      var data;
-      $(this).hide();
-      $('#notetaking input.cancel').hide();
-
-      data = {
-        subject: $(subject).val(),
-        note: $(text).val()
-      };
-
-      if (note) {
-        data.id = note.id;
-        note = null;
-        noteapi.update(data, note_saved);
-      }
-      else {
-        noteapi.create(data, note_saved);
-      }
-      note = null;
-    });
-
-    note_saved = function (res) {
-      note = null;
-      $(subject).val('');
-      $(text).val('');
-      $('#notetaking input.save').show();
-
-      noteapi.retreive(res.id, function (res) {
-        note_append(res);
+      var tm = new $jit.TM.Squarified({
+        injectInto: 'citybudget-js-treemap',
+        animate: true,
       });
+      tm.loadJSON(treemap);
+      tm.refresh();
+    });
+
+    citybudget_append = function (programData) {
+      var location = this.location.href + '/';
+      $('<li class="program">' +
+        '<a href="' + location + programData.name + '">' + programData.name +
+        '</a> ' + programData['2009 Budget'] +
+        '</li>').appendTo('#citybudget-js');
     };
-*/
+
+    
   });
 }(jQuery));
